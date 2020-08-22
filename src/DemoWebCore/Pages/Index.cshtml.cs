6e6bbb11078bdc55c047da546d40b4a1fb55b31f
Jeffrey Lee <jeffrey@darkthread.net>
2020-08-22 16:03:36 +0800

    add - ASP.NET Core version prototype (Windows-only)

---
+++ src/DemoWebCore/Pages/Index.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace DemoWebCore.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPostBusyLoop()
+        {
+            var timeout = DateTime.Now.AddSeconds(5);
+            while (DateTime.Now.CompareTo(timeout) < 0)
+            {
+                var newGuid = Guid.NewGuid();
+            }
+        }
+
+        static List<byte[]> MemoryMonster = new List<byte[]>();
+
+        public void OnPostUseMemory()
+        {
+            byte[] buff = new byte[8 * 1024 * 1024];
+            for (var i = 0; i < buff.Length; i++) buff[i] = (byte)(i % 256);
+            MemoryMonster.Add(buff);
+            //Sleep 5 seconds to keep the memory space "active"
+            Thread.Sleep(5000);
+        }
+    }
+}
---
+++ src/DemoWebCore/Pages/WebStatsMonitor/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+using System.Threading.Tasks;
+using DemoWebCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DemoWebCore.Pages.WebStatsMonitor
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+
+        public IActionResult OnGetStats()
+        {
+            return new JsonResult(new
+            {
+                CPU = SelfPerfMonitor.GetCpuUsage(),
+                RAM = SelfPerfMonitor.GetMemUsage()
+            }, new System.Text.Json.JsonSerializerOptions()
+            {
+                PropertyNamingPolicy = null,
+                Encoder =
+                    JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.CjkUnifiedIdeographs)
+            });
+        }
+    }
+}
